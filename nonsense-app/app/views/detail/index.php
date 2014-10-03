616936b0299e24d6475cea9ddb9d448c7b4a06b9
annd <annd@vng.com.vn>
2014-10-03 16:44:08 +0700

    update detail

--- nonsense-app/app/views/detail/index.php
+++ nonsense-app/app/views/detail/index.php
@@ -6,25 +6,23 @@
 				<article class="badge-entry-container badge-entry-entity">
 					<header>
 						<h2 class="badge-item-title">
-							<a class="badge-evt badge-track" href="javascript:void(0)"> <?php echo $data['posts']['content']['caption']?> </a>
+						  <a class="badge-evt badge-track" href="javascript:void(0);">
+						     <?php echo $data['posts']['content']['caption']?>
+						  </a>
 						</h2>
 					</header>
 					<div class="badge-post-container post-container">
-						<a href="<?php echo $data['posts']['post_url']?>" class="badge-animated-cover badge-track badge-track-no-follow" style="max-height:500px;padding-left:32px;"> 							
-							<div class="badge-animated-container-static gif-post presenting">
-								<img height="<?php echo $data['posts']['content']['images']['medium']['height']?>" width="<?php echo $data['posts']['content']['images']['medium']['width']?>"  class="badge-item-img" src="<?php echo $data['posts']['content']['images']['medium']['url']?>" alt="<?php echo $data['posts']['content']['caption']?>" />
-							</div> 
+						<a href="javascript:void(0);">
+                            <img class="badge-item-img" src="<?php echo ($data['posts']['type'] == 'image' ? $data['posts']['content']['images']['medium']['url'] : $data['posts']['content']['image_url']) ?>" alt="<?php echo $data['posts']['image']['caption']?>" />
 						</a>
 					</div>
-					<p class="post-meta"> 
-						<a class="badge-evt point" id="love-count-aAVP4Go" href="javascript:void(0);"> 
-							<span class="badge-item-love-count">0</span> points
-						</a> &middot; 
+					<p class="post-meta">
+						<a class="badge-evt point" href="javascript:void(0)"><span class="badge-item-love-count">0</span> points</a> &middot;
 						<a class="comment badge-evt" href="javascript:void(0);">
 							<fb:comments-count href="<?php echo $data['posts']['post_url']?>"/></fb:comments-count> comments
 						</a>
 					</p>
-					<div class="badge-item-vote-container post-afterbar-a in-list-view">
+					<div class="badge-item-vote-container post-afterbar-a in-list-view  ">
 						<div class="vote">
 							<ul class="btn-vote left">
 								<li>
@@ -38,13 +36,13 @@
 						<div class="share right">
 							<ul>
 								<li>
-									<a href="javascript:void(0);" class="badge-facebook-share badge-evt badge-track btn-share facebook" data-share="http://9gag.com/gag/aAVP4Go?ref=fb.s">Facebook</a>
+									<a href="javascript:void(0);" class="badge-facebook-share badge-evt badge-track btn-share facebook" data-share="https://www.facebook.com/pages/G%C3%B3c-H%C3%A0i/281158712077617">Facebook</a>
 								</li>								
 							</ul>
 						</div>
 						<div class="clearfix"></div>
 					</div>
-				</article>					
+				</article>													
 			<?php } ?>			
 		</div>		
 	</section>
